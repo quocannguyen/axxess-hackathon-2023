ca2007771a43157f5382394a4cde1456635f913c
quocannguyen <78980126+quocannguyen@users.noreply.github.com>
2023-04-01 15:15:08 -0500

    Create enum Genders. Create function getDiagnosis(symptomIds, gender, yearOfBirth) to fetch from api.

---
+++ api/apimedic.js
@@ -0,0 +1,24 @@
+const AUTH_TOKEN = process.env.AUTH_TOKEN
+
+if (!AUTH_TOKEN) {
+    throw new Error("Please define the AUTH_TOKEN inside .env.local")
+}
+
+const Genders = {
+    Male: "male",
+    Female: "female"
+}
+
+function getDiagnosis(symptomIds, gender, yearOfBirth) {
+    let url = `https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[${symptomIds.toString()}]&gender=${gender}&year_of_birth=${yearOfBirth}&token=${AUTH_TOKEN}&format=json&language=en-gb`
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(response => {
+            console.log(JSON.stringify(response))
+        })
+}
